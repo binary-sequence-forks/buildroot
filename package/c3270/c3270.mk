5020e4c9baef88eda72afa66f2c0c2820d8b13f9
Sergio Lindo Mansilla <sergiolindo.empresa@gmail.com>
2025-01-05 14:44:14 +0100

    Add package c3270

---
+++ package/c3270/c3270.mk
@@ -0,0 +1,17 @@
+C3270_VERSION = 4.3ga10
+C3270_SITE = https://github.com/pmattes/x3270.git
+C3270_SITE_METHOD = git
+
+define C3270_CONFIGURE_CMDS
+	cd $(@D) && ./configure --enable-c3270
+endef
+
+define C3270_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) c3270
+endef
+
+define C3270_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/c3270 $(TARGET_DIR)/usr/bin/c3270
+endef
+
+$(eval $(generic-package))
